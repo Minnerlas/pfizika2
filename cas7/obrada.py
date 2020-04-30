9bf59ca4f6d90574dde1a99a9aed2f81d2e623cb
vodnikangus <petar.radojevic22@gmail.com>
2020-04-30 22:09:58 +0200

    asd

---
+++ cas7/obrada.py
@@ -0,0 +1,18 @@
+#!/bin/python
+
+# Nikola Radojević 2019/176
+
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import linspace
+from scipy.integrate import odeint
+from scipy.optimize import fsolve
+from math import sqrt
+from os import system
+
+_=system("clear")
+
+
+from podaci import *
+
+print(xc)
